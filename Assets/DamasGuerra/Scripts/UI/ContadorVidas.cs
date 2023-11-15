bb38d5afe2a9804f30ef1b8bcab89b9583263394
Sara Garavito Posada <saragaravitoposada@gmail.com>
2023-11-15 15:58:45 -0500

    contador vidas

--- Assets/DamasGuerra/Scripts/UI/ContadorVidas.cs
+++ Assets/DamasGuerra/Scripts/UI/ContadorVidas.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using MoreMountains.TopDownEngine;
 using TMPro;
 using UnityEngine;
+using MoreMountains.Tools;
 
+
+//script para actualizar las vidas 
 public class ContadorVidas : MonoBehaviour
 {
     [SerializeField]
@@ -15,9 +18,18 @@ public class ContadorVidas : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    //mayor que 0 sigue contando vidas sino, se acaba el juego
     void Update()
     {
-        contador.SetText(GameManager.Instance.CurrentLives.ToString());
+        if (GameManager.Instance.CurrentLives <0 )
+        {
+            contador.SetText(GameManager.Instance.CurrentLives.ToString());
+        }
+        else
+        {
+            //MMGameEvent.trigger("JuegoTerminado");
+            MMSceneLoadingManager.LoadScene("Perdio");
+        }
+        
     }
 }
